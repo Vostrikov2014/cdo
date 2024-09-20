01c759e3db0aa265d28c0227519068b1b2d2af73
vostr <vostrlog@gmail.com>
2024-09-20 08:02:04 +0300

    Initial commit

---
+++ cdo-back/cdo-back/src/main/java/com/example/cdoback/CdoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdoBackApplication.class, args);
+    }
+
+}
---
+++ cdo-back/cdo-back/src/test/java/com/example/cdoback/CdoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
